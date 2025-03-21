36ac8f53a540c67eb12eeb61816c30cfdd3b7f95
Vincius Woloszyn <vinicius.woloszyn@de.bosch.com>
2025-03-21 14:07:38 +0100

    formatting with black

--- LLMEval/EvalLLM.py
+++ LLMEval/EvalLLM.py
@@ -1,30 +1,32 @@
-import csv, os
+import csv
+import os
+
 import pandas as pd
 import ResourcesChatGPT as rChatGPT
 
 
 def __prepro_diagnostics(diagnostics):
-	new_diagnostics, new_set_diagnostics = [], []
-	for set_diagnostics in diagnostics:
-		splited = set_diagnostics.split('*')
+    new_diagnostics, new_set_diagnostics = [], []
+    for set_diagnostics in diagnostics:
+        splited = set_diagnostics.split("*")
 
-		for diagnostic in splited:
-			if diagnostic != '':
-				new_set_diagnostics.append('*'+diagnostic)
-		new_diagnostics.append(new_set_diagnostics)
-		new_set_diagnostics = []
+        for diagnostic in splited:
+            if diagnostic != "":
+                new_set_diagnostics.append("*" + diagnostic)
+        new_diagnostics.append(new_set_diagnostics)
+        new_set_diagnostics = []
 
-	return new_diagnostics
+    return new_diagnostics
 
 
 def prompting(gt, llm, output_file_path):
-	responses = []
-	gt_to_csv, llm_to_csv = [], []
-	i = 1
-	for diagnostics_gt, diagnostics_llm in zip(gt, llm):
-		txt_gt = '\n'.join(diagnostics_gt)
-		txt_llm = '\n'.join(diagnostics_llm)
-		prompt = f"""[Instrução] A tarefa é comparar a lista "referência" com a lista "hipótese". A lista "referência" é curada, anotada por especialistas, e não possui repetições ou diagnósticos similares. Todos os diagnósticos, fatores de risco ou procedimentos da lista "referência" devem estar presentes na lista "hipótese".
+    responses = []
+    gt_to_csv, llm_to_csv = [], []
+    i = 1
+    for diagnostics_gt, diagnostics_llm in zip(gt, llm):
+        txt_gt = "\n".join(diagnostics_gt)
+        txt_llm = "\n".join(diagnostics_llm)
+        prompt = f"""[Instrução] A tarefa é comparar a lista "referência" com a lista "hipótese". A lista "referência" é curada, anotada por especialistas, e não possui repetições ou diagnósticos similares. Todos os diagnósticos, fatores de risco ou procedimentos da lista "referência" devem estar presentes na lista "hipótese".
 
 Lista “referência”:
 {txt_gt}
@@ -47,49 +49,50 @@ Para calcular a precisão e o recall:
 ### Resultados:
 Mostre a precisão e o recall no formato: "[[Precisão, Recall]]". Por exemplo, se a precisão for 0.8 e o recall for 0.7, o formato seria "[[0.8, 0.7]]"
 """
-		
-		response = rChatGPT.get_response(prompt)
-		responses.append(response)
-		
-		print(f' :: Processed {i}/{len(gt)}')
-		
-		gt_to_csv.append('\n'.join(diagnostics_gt))
-		llm_to_csv.append('\n'.join(diagnostics_llm))
 
-		i+=1
+        response = rChatGPT.get_response(prompt)
+        responses.append(response)
 
-	data = {
-		"GT": gt_to_csv,
-		"LLM": llm_to_csv,
-		"RESPONSES": responses,
-	}
+        print(f" :: Processed {i}/{len(gt)}")
 
-	df = pd.DataFrame(data)
-	
-	df.to_csv(output_file_path, sep='\t', index=False, quoting=csv.QUOTE_NONNUMERIC, quotechar='"')
+        gt_to_csv.append("\n".join(diagnostics_gt))
+        llm_to_csv.append("\n".join(diagnostics_llm))
 
-	print("-----------------------")
-	
-if __name__ == '__main__':
-	drop_list = [0, 9, 11]
+        i += 1
 
-	df_gt = pd.read_csv('./data/gt.tsv', sep='\t')
-	gt = df_gt['GT'].values.tolist()
-	gt = __prepro_diagnostics(gt)
+    data = {
+        "GT": gt_to_csv,
+        "LLM": llm_to_csv,
+        "RESPONSES": responses,
+    }
 
-	for file in os.listdir('./data/'):
-		if 'diag' in file:
-			if os.path.exists('./result-gpt4o/'+file):
-				print('[Ignoring] File already processed', file, '...', flush=True)
-				continue
+    df = pd.DataFrame(data)
 
-			print('Processing file', file, '...', flush=True)
+    df.to_csv(output_file_path, sep="\t", index=False, quoting=csv.QUOTE_NONNUMERIC, quotechar='"')
 
-			df_diag = pd.read_csv('./data/' + file, sep='\t')
-			df_diag = df_diag.drop(drop_list)
+    print("-----------------------")
 
-			llm_diag = df_diag['DIAGNOSTICOS_LLM'].values.tolist()
 
-			diagnostics_llm = __prepro_diagnostics(llm_diag)
-			
-			prompting(gt, diagnostics_llm, './result-gpt4o/'+file)
+if __name__ == "__main__":
+    drop_list = [0, 9, 11]
+
+    df_gt = pd.read_csv("./data/gt.tsv", sep="\t")
+    gt = df_gt["GT"].values.tolist()
+    gt = __prepro_diagnostics(gt)
+
+    for file in os.listdir("./data/"):
+        if "diag" in file:
+            if os.path.exists("./result-gpt4o/" + file):
+                print("[Ignoring] File already processed", file, "...", flush=True)
+                continue
+
+            print("Processing file", file, "...", flush=True)
+
+            df_diag = pd.read_csv("./data/" + file, sep="\t")
+            df_diag = df_diag.drop(drop_list)
+
+            llm_diag = df_diag["DIAGNOSTICOS_LLM"].values.tolist()
+
+            diagnostics_llm = __prepro_diagnostics(llm_diag)
+
+            prompting(gt, diagnostics_llm, "./result-gpt4o/" + file)
